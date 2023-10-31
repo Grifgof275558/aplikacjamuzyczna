416216c876479dcfdf1522022d74c4b26d5ac224
SAPO <SAPO@LAPTOP-IRJ3LL03>
2023-10-31 12:51:39 +0100

    Dodaj pliki projektów.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace aplikacjamuzyczna
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
